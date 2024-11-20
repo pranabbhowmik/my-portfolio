83ebf39519496759a6eef3ae537a206971ff1da2
Pranab Bhowmik <109957935+Pranab09122002@users.noreply.github.com>
2024-11-20 16:47:38 +0530

    Add new dependencies and update project structure with new assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
-import React, { useState } from 'react'
-import Hero from './section/Hero/Hero'
-import Project from './section/Projects/Project'
-import Skill from './section/Skills/Skill';
-import Education from './section/Education/Education';
-import Contact from './section/Contact/Contact';
-import Footer from './section/Footer/Footer';
+import "./App.css";
+import React, { useState } from "react";
+import Hero from "./section/Hero/Hero";
+import Project from "./section/Projects/Project";
+import Education from "./section/Education/Education";
+import Contact from "./section/Contact/Contact";
+import Footer from "./section/Footer/Footer";
+import Skill from "./section/Skills/Skill";
 
 const App = () => {
   // Move the mode state and handelmode function to the parent component
@@ -16,15 +16,19 @@ const App = () => {
   };
 
   return (
-    <div className={mode === "light" ? "bg-white text-black" : "bg-gray-800 text-white"}>
+    <div
+      className={
+        mode === "light" ? "bg-white text-black" : "bg-gray-800 text-white"
+      }
+    >
       {/* Pass mode and handleMode as props to both Hero and Project */}
       <Hero mode={mode} handelmode={handelmode} />
       <Project mode={mode} />
-      <Skill mode={mode}/>
-      <Education mode={mode}/>
-      <Contact mode={mode}/>
-      <Footer  mode={mode}/>
+      <Skill mode={mode} />
+      <Education mode={mode} />
+      <Contact mode={mode} />
+      <Footer mode={mode} />
     </div>
   );
 };
-export default App
+export default App;
--- src/section/Projects/Project.jsx
+++ src/section/Projects/Project.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Projectcard from "../../Card/Projectcard";
-import movie from "../../assets/viberr.png";
+import goppo from "../../assets/goppo.png";
 import totowala from "../../assets/totowala.jpg";
-import fakestore from "../../assets/Fake.jpg";
-import pokemon from "../../assets/pokemon.jpg";
+import Saregamapa from "../../assets/saregamapa.png";
+import note from "../../assets/note.jpg";
 
 const Project = ({ mode }) => {
   return (
     <section
       id="projects"
       className={`flex flex-col gap-8 scroll-smooth items-center md:flex-row md:flex-wrap min-h-[1580px] md:min-h-lvh md:justify-evenly md:px-8 
-      -m-1 md:-mt-32 ${mode === "light" ? "bg-white text-black" : "bg-black text-white"}`}
+      -m-1 md:-mt-32 ${
+        mode === "light" ? "bg-white text-black" : "bg-black text-white"
+      }`}
     >
       <h1 className="text-5xl font-rubik mb-8">PROJECTS</h1>
       <div className="flex flex-col gap-28 md:gap-16 items-center md:flex-row md:flex-wrap md:justify-evenly md:px-8 md:-mt-48 mt-3">
         <Projectcard
-          src={movie}
-          link="https://moviehub-tukai.netlify.app/"
-          h3="Movie-Hub"
-          p="Movie App"
+          src={goppo}
+          link="https://goppo.onrender.com/signup"
+          h3="Goppo"
+          p="Chat App"
         />
         <Projectcard
           src={totowala}
@@ -27,16 +29,16 @@ const Project = ({ mode }) => {
           p="E-rickshaw booking"
         />
         <Projectcard
-          src={fakestore}
-          link="https://spotify-tukai.netlify.app/"
-          h3="Fake Store"
-          p="E-commerce"
+          src={Saregamapa}
+          link="https://saregamapadha.netlify.app/"
+          h3="Sa Re Ga Ma Pa"
+          p="Music App"
         />
         <Projectcard
-          src={pokemon}
-          link="https://pokemon-tukai18.netlify.app/"
-          h3="Pokemon"
-          p="Pokemons"
+          src={note}
+          link="https://notebanao.netlify.app/"
+          h3="Note Banao"
+          p="Note App"
         />
       </div>
     </section>
